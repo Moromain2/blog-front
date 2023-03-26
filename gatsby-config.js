88c43aecc5e55961533c740dd80bec2f5c8afab8
Romain Mosser <mosser.romain@gmail.com>
2023-03-26 16:43:08 +0200

    Set up Prismic back-end

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,39 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const routes = [
+  {
+    type: "blog_post",
+    path: "/blog_post/:uid",
+  },
+  {
+    type: "home",
+    path: "/",
+  },
+];
+
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        routes,
+      },
+    },
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,61 @@
 import * as React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const IndexPage = () => {
+export const query = graphql`
+  query {
+    prismicHome {
+      data {
+        hero_title {
+          text
+        }
+        hero_text {
+          text
+        }
+      }
+    }
+    allPrismicBlogPost {
+      nodes {
+        uid
+        data {
+          publication_date
+          featured
+          title {
+            text
+          }
+          body {
+            text
+          }
+          cover {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = ({data}) => {
+  const homePageData = data.prismicHome.data
+  const blogPostsData = data.allPrismicBlogPost.nodes
+  console.log(blogPostsData)
   return (
     <main>
-      <h1>Home</h1>
+      <h1>{homePageData.hero_title.text}</h1>
+      <p>{homePageData.hero_text.text}</p>
+      <div>
+        {blogPostsData.map((post) => (
+          <div className="post-card" key={post.uid}>
+            <h2>{post.data.title.text}</h2>
+            <time dateTime={post.data.publication_date}>{post.data.publication_date}</time>
+            <GatsbyImage
+              image={getImage(post.data.cover)}
+              alt={post.data.title.text}
+            />
+            <p>{post.data.body.text.slice(0, 150)}...</p>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
